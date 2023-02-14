335b17ef175afb8aa5b825e0ebd8604a0af17914
Vladyslav <vkadyrov@protonmail.com>
2023-02-14 14:53:56 -0500

    commit tree b342adce6c3b463e05fa6224d049daa35f849196

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+
+    attr_accessor :equilateral, :isosceles, :scalene
+
+    def initialize(a, b, c)
+      @a=a
+      @b=b
+      @c=c
+    end
+
+    def kind
+      if (@a >= 0 && @b >= 0 && @c >= 0) && ((@a+@b) > @c && (@c+@b) > @a && (@a+@c) > @b)   
+        if @a==@b && @a==@c
+          :equilateral
+        elsif @a==@b || @c==@b || @a==@c
+          :isosceles  
+        elsif @a!=@b && @a!=@c
+          :scalene 
+        end
+      else 
+        raise TriangleError    
+      end
+    end
+
+    class TriangleError < StandardError
+    end
+
 end
